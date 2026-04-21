11a5ee12772fa027a2a53b5831a8bd9916897173
Christophe Vanlancker <carroarmato0@gmail.com>
2026-04-21 22:31:49 +0200

    fix(settings): update NewSettingsScreen call sites in screen_detail.go

--- internal/ui/screen_detail.go
+++ internal/ui/screen_detail.go
@@ -400,7 +400,7 @@ func (s *DetailScreen) HandleEvent(e sdl.Event) Screen {
 				return s.startDownload()
 			}
 		case sdl.K_s:
-			return NewSettingsScreen(s.cfg, s.cfgPath, s)
+			return NewSettingsScreen(s.cfg, s.cfgPath, s, nil)
 		}
 	case *sdl.ControllerButtonEvent:
 		switch ev.Button {
@@ -438,7 +438,7 @@ func (s *DetailScreen) HandleEvent(e sdl.Event) Screen {
 				return s.startDownload()
 			}
 		case sdl.CONTROLLER_BUTTON_START:
-			return NewSettingsScreen(s.cfg, s.cfgPath, s)
+			return NewSettingsScreen(s.cfg, s.cfgPath, s, nil)
 		}
 	case *sdl.QuitEvent:
 		return nil
